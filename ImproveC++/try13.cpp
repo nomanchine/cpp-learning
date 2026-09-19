24023eb66e47cc3a77d72290c20c1381c920784f
nomanchine <3187063712@qq.com>
2026-09-19 15:17:03 +0800

    (list案例)

---
+++ ImproveC++/try13.cpp
@@ -0,0 +1,65 @@
+
+class Person
+{
+public:
+	Person(string name,int age,int height)
+	{
+		this->m_age = age;
+		this->m_height = height;
+		this->m_name = name;
+	}
+
+	string m_name;
+	int m_age;
+	int m_height;
+};
+
+bool compare(Person &p1,Person &p2)
+{
+	if(p1.m_age == p2.m_age)
+	{
+		return p1.m_height < p2.m_height;
+	}
+	else
+	{
+		return p1.m_age < p2.m_age;
+	}
+}
+
+int main()
+{
+	list<Person>L;
+
+	Person p1("刘备", 35, 175);
+	Person p2("曹操", 45, 180);
+	Person p3("孙权", 40, 170);
+	Person p4("赵云", 25, 190);
+	Person p5("张飞", 35, 160);
+	Person p6("关羽", 35, 200);
+
+	L.push_back(p1);
+	L.push_back(p2);
+	L.push_back(p3);
+	L.push_back(p4);
+	L.push_back(p5);
+	L.push_back(p6);
+
+
+	cout << "排序前：" << endl;
+	for(list<Person>::iterator it = L.begin();it != L.end();it++)
+	{
+		cout << "姓名：" << it->m_name << " 年龄：" << (*it).m_age << " 身高：" << it->m_height << endl;
+	}
+	cout <<" --------------------------------------------" << endl;
+	cout << "排序后：" << endl;
+
+	L.sort(compare);
+	for (list<Person>::iterator it = L.begin();it != L.end();it++)
+	{
+		cout << "姓名：" << it->m_name << " 年龄：" << (*it).m_age << " 身高：" << it->m_height << endl;
+	}
+
+
+	system("pause");
+	return 0;
+}
